19c122e19743c39d569c0cd9cfe74b55d07a7258
dianeyasminec <dianeyasminec60@gmail.com>
2021-06-21 15:12:12 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+function saturdayFun (activity="roller-skate"){
+ return "This Saturday, I want to "+activity+"!"
+}
+
+
+function mondayWork(activity="go to the office"){
+    return "This Monday, I will "+activity+"."
+}
+
+function wrapAdjective (string="*"){
+    
+        return function(string2){
+            const var1="You are"
+            
+            return var1+" "+string+string2+string+"!"
+            
+
+        }
+    
+
+}
+let result=wrapAdjective()
+let emphatic=result("a hard worker")
+console.log(emphatic)
